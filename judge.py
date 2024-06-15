d89e137d1e7e8d4304d460df30a0d95f7cf485ca
Niklas Wretblad <niklas.wretblad@liu.se>
2024-06-15 08:23:17 +0200

    fix

--- judge.py
+++ judge.py
@@ -141,10 +141,10 @@ if __name__ == "__main__":
     logging.getLogger("urllib3").setLevel(logging.WARNING)
 
     MODEL_NAME = "gpt-4o"
-    GOLD_DATASET_PATH = "output/GOLD_DATASET_FINAL.csv"
+    GOLD_DATASET_PATH = "output/GOLD_DATASET_FINAL copy.csv"
     MODEL_PREDICTIONS_PATH = "output/col_desc_pred/Pred_DEV_desc_mixtral-8x22.csv"
 
-    OUTPUT_PATH = "output/col_desc_pred/Pred_DEV_desc_mixtral-8x22.csv"
+    OUTPUT_PATH = "output/judge/judge_mistral_22x8.csv"
 
     model = LLMInterface(MODEL_NAME)
 
@@ -165,9 +165,12 @@ if __name__ == "__main__":
         if (index in processed_indexes):
             print(f"Skipping already processed description number: {index}")
             continue
-                    
+        
+        if index == 798:
+            continue
+        
         predicted_descr = row['llm_column_description']
-        gold_descr = gold_df.loc[index, 'column_description']
+        gold_descr = gold_df.at[index, 'column_description']
 
         result = ""
         while result == "":
